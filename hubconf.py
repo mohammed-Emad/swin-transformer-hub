24a0b6a2ad4bc376d90501e935d71a308911bfaa
mohammed-Emad <mohammed.imad38@yahoo.com>
2024-01-23 14:40:31 +0200

    Update hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -9,7 +9,7 @@ MODEL_URLS = {
 }
 
 def swin_tiny_patch4_window7_224(pretrained=False):
-     model = swin.SwinTransformer(img_size=224, patch_size=4, windows_size=7, depths=[2, 2, 18, 2], embed_dim=128, num_heads=[4, 8, 16, 32])
+    model = swin.SwinTransformer(img_size=224, patch_size=4, windows_size=7, depths=[2, 2, 18, 2], embed_dim=128, num_heads=[4, 8, 16, 32])
     if pretrained:
         model.load_state_dict(torch.hub.load_state_dict_from_url(MODEL_URLS["swin_tiny_patch4_window7_224"], map_location="cpu")['model'])
     return model
